fbef6d9809c8d08be585cbdeb28fc93cf52352cd
Eduardo Duarte <hi@edduarte.com>
2020-01-21 16:44:17 +0000

    Updated gulpfile and other dependencies to be compatible with Gulp 4.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,30 +2,27 @@ import gulp from 'gulp'
 import htmlmin from 'gulp-htmlmin'
 import cleanCSS from 'gulp-clean-css'
 import minify from 'gulp-babel-minify'
-import runSequence from 'run-sequence'
 import shell from 'gulp-shell'
 import purgecss from 'gulp-purgecss'
 import replace from 'gulp-replace'
 import fs from 'fs'
 
-gulp.task('hugo-build', shell.task(['hugo']))
-
-gulp.task('purgecss', () => {
+const purgeCSS = () => {
   return gulp.src('public/**/*.css')
     .pipe(purgecss({
       content: ['public/**/*.html']
     }))
     .pipe(gulp.dest('./public'));
-});
+};
 
-gulp.task('add-inline-css', () => {
+const addInlineCSS = () => {
   const fileContent = fs.readFileSync("public/css/main.css", "utf8");
   return gulp.src('public/**/*.html')
     .pipe(replace('<!-- INLINE_CSS -->', fileContent))
     .pipe(gulp.dest('./public'));
-});
+};
 
-gulp.task('minify-html', () => {
+const minifyHTML = () => {
   return gulp.src('public/**/*.html')
     .pipe(htmlmin({
       collapseWhitespace: true,
@@ -35,15 +32,15 @@ gulp.task('minify-html', () => {
       useShortDoctype: true,
     }))
     .pipe(gulp.dest('./public'));
-});
+};
 
-gulp.task('minify-css', () => {
+const minifyCSS = () => {
   return gulp.src('public/**/*.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('./public'));
-});
+};
 
-gulp.task('minify-js', () => {
+const minifyJS = () => {
   return gulp.src('public/**/*.js')
     .pipe(minify({
       mangle: {
@@ -51,8 +48,8 @@ gulp.task('minify-js', () => {
       }
     }))
     .pipe(gulp.dest('./public'));
-});
+};
 
-gulp.task('build', ['hugo-build'], (callback) => {
-  runSequence('minify-css', 'purgecss', 'add-inline-css', 'minify-html', 'minify-js', callback);
-});
+const build = gulp.series(minifyCSS, purgeCSS, addInlineCSS, gulp.parallel(minifyHTML, minifyJS));
+exports.build = build;
+exports.default = build;
